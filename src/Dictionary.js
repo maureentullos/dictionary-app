1847bec72b9c17a3b24a64a42b086edef2f458d3
maureentullos <Mswalsh220@gmail.com>
2024-02-08 20:46:39 -0500

    Started to add dictionary search.

---
+++ src/Dictionary.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Dictionary.css";
+
+
+export default function Dictionary() {
+    return <div className="Dictionary">
+        <form>
+            <input type="search"/>
+        </form>
+    </div>;
+}
